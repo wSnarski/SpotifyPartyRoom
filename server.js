a50ccd1f1791acf4078b96eb7bdee3a04c7d8d1b
Wojtek Snarski <wsnarski@carthage.edu>
2016-04-02 12:43:28 -0500

    Un breaking out controllers

--- server.js
+++ server.js
@@ -112,6 +112,21 @@ app.get('/api/me', ensureAuthenticated, function(req, res) {
   });
 });
 
+app.post('/api/rooms', _auth, function(req, res, next) {
+  var Room = new Rooms({
+    name: req.body.name,
+    owner: req.user.dbId
+  }).save(function(err, nRoom){
+    if (err) return next(err);
+    res.location('/api/rooms/' + nRoom.id.toString());
+    res.status(201).send(nRoom);
+  });
+});
+
+app.get('/api/me/rooms', ensureAuthenticated, function(req, res, next) {
+
+});
+
 
 //TODO esnureAuthenticated can be required in instead of passed in
 //require('./controllers/roomsController')(app, Rooms, ensureAuthenticated);
